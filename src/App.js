661fce8c506dbf9d13eeb10730fb8249b279df7e
Sathish7Kumar <sathishnagarajan1996@gmail.com>
2024-03-06 23:15:25 +0530

    v.1.1 - 06.03

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useEffect, useState } from 'react'
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import './App.css'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Navbar from './pages/Navbar'
+import axios from 'axios'
+import Home from './pages/Home'
+const url = "https://fakestoreapi.com/products"
 
-function App() {
+export const FakeContext = createContext()
+
+// useContext - create , provide , useContext
+
+
+const App = () => {
+
+  const [data, setdata] = useState([])
+  const [cart, setcart] = useState([])
+  const [cartValue, setcartValue] = useState(0)
+
+  useEffect(()=>{
+    getData()
+  })
+
+  const getData = async () =>{
+    const response = await axios.get(url)
+    setdata(response.data)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Router>
+      <FakeContext.Provider value={{data,setdata,cart,setcart,cartValue,setcartValue}}>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/products' element={<Product/>} />
+        <Route path='/cart' element={<Cart/>} />
+      </Routes>
+      </FakeContext.Provider>
+    </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
